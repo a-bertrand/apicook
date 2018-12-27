1b8e0ce938577c09821bd3c8f7166d02d274f7a8
a-bertrand <antho.bertrand101@gmail.com>
2018-12-27 19:31:38 +0100

    update serializers

--- apicook/apicook/cookie/serializers.py
+++ apicook/apicook/cookie/serializers.py
@@ -3,22 +3,19 @@ from rest_framework import serializers
 from .models import Article, Ingredient, Recipe
 
 
-class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField()
+class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
 
 
-class IngredientSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField()
+class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = '__all__'
 
 
-class RecipeSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField()
+class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = '__all__'
